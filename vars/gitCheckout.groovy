d6f10ea11954f8aae2fae97407648bc12046b3ff
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2019-11-30 14:59:36 +0200

    delete examples and make ecsDeploy file

--- vars/gitCheckout.groovy
+++
@@ -1,9 +0,0 @@
-
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
