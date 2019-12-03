857b6f339322342b8d4794e266a6aa9b6ebd2954
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2019-12-03 17:26:24 +0200

    correction of new script

--- vars/repositoryDownload.groovy
+++ vars/repositoryDownload.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/groovy
 
-sh"""
-echo "Clone github repository"
-git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
-ls -al jenkins-shared-library-structure
-"""
+def call ()
+{
+	sh"""
+	echo "Clone github repository"
+	git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
+	ls -al jenkins-shared-library-structure
+	"""
+}
