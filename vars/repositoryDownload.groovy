8de3eab1d4620d262c5aa4c1d5f9b7cdedde86ea
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2019-12-03 17:45:13 +0200

    correction of ls command

--- vars/repositoryDownload.groovy
+++ vars/repositoryDownload.groovy
@@ -5,7 +5,7 @@ def call ()
 	sh"""
 	echo "Delete old copy of github repository"
 	rm -r jenkins-shared-library-structure
-	ls -al jenkins-shared-library-structure
+	ls -a
 	echo "Clone github repository"
 	git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
 	ls -al jenkins-shared-library-structure
