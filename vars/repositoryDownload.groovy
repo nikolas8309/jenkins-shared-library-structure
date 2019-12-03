fe8aeef3a800f25fcda17315167ca49fe8eb777a
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2019-12-03 17:31:39 +0200

    debug downloaded repo 2

--- vars/repositoryDownload.groovy
+++ vars/repositoryDownload.groovy
@@ -4,8 +4,8 @@ def call ()
 {
 	sh"""
 	echo "Clone github repository"
-	git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
 	pwd
+	git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
 	ls -al jenkins-shared-library-structure
 	"""
 }
