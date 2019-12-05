6e88ac4cfd3af41779a67c80e6b3a616ec9a962a
nikolas8309 <nikolaos.stamatelopoulos@gmail.com>
2019-12-05 13:40:03 +0200

    solve the building stop problem if dir is not there

--- vars/repositoryDownload.groovy
+++ vars/repositoryDownload.groovy
@@ -4,7 +4,7 @@ def call ()
 {
 	sh"""
 	echo "Delete old copy of github repository"
-	rm -r jenkins-shared-library-structure
+	rm -r jenkins-shared-library-structure || true
 	ls -a
 	echo "Clone github repository"
 	git clone https://github.com/nikolas8309/jenkins-shared-library-structure.git
